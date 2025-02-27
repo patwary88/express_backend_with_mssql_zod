9b5ca7cea9dd07c409aac3b74d7d7d5b2ed9557b
patwary88 <mainuddinpatwary1988@gmail.com>
2025-02-27 08:37:19 +0600

    try to create mssql connection

--- src/controllers/MssqlController.ts
+++ src/controllers/MssqlController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { MssqlService } from '../services/MssqlService';
-
+import { sql, poolPromise } from "../config/mssqldb";
 export class MssqlController {
   private mssqlService: MssqlService;
 
@@ -9,10 +9,22 @@ export class MssqlController {
     this.mssqlService = new MssqlService();
   }
 
-  funcGetRawData = async (req: Request, res: Response): Promise<void> => {
+  funcGetRawData = async (req: Request, res: Response): Promise<any> => {
 
-    console.log(req);return;
-    const RawData =  await this.mssqlService.getDataList();
+
+    try {
+      const pool = await poolPromise; // Now properly typed
+      const result = await pool.request().query("SELECT TOP (1000) * FROM NGAC_AUTHLOG ");
+      console.log(result);return;
+
+      res.json(result.recordset);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Database query failed" });
+  }
+
+
+    //const RawData =  await this.mssqlService.getDataList();
   }
 
 }
--- src/services/MssqlService.ts
+++ src/services/MssqlService.ts
@@ -1,10 +1,18 @@
-import { UserRepository } from '../repositories/UserRepository';
+//import { UserRepository } from '../repositories/UserRepository';
 import logger from '../utils/logs/logger';
-
+import { sql, poolPromise } from "../config/mssqldb";
 export class MssqlService {
 
-    async getDataList(): Promise<any> {
+    // async getDataList(): Promise<any> {
 
+    //     try {
+    //         const pool = await poolPromise; // Now properly typed
+    //         const result = await pool.request().query("SELECT * FROM Users");
+    //         res.json(result.recordset);
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.status(500).json({ error: "Database query failed" });
+    //     }
 
-    }
+    // }
 }
